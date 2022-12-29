870495f240c07613d55729dee40374e76db8a26f
Nathan Pham <nathanpham.me@gmail.com>
2022-12-28 22:15:00 -0800

    wrap predictions

--- src/components/Completion.tsx
+++ src/components/Completion.tsx
@@ -8,7 +8,9 @@ const Completion = ({ completion, english }: CompletionProps) => {
         <div className="p-4 rounded-lg border mt-4">
             <p>{english}</p>
             <code className="font-mono p-4 bg-gray-50 border rounded-lg mt-2 w-full block">
-                <pre>{completion}</pre>
+                <pre className="break-words whitespace-pre-wrap">
+                    {completion}
+                </pre>
             </code>
         </div>
     );
